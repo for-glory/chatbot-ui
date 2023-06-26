bedbb016ed16efc2d0cf98b7bc641b035fa703be
for-glory <forglory0905@outlook.com>
2023-06-26 03:14:30 -0700

    add auth callback feature

---
+++ pages/auth/callback/callback.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+// import { useRouter } from 'next/navigation'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+export default function Callback() {
+  const router = useRouter()
+  const supabase = createClientComponentClient()
+
+  useEffect(() => {
+    const { code } = router.query
+    const exchangeCodeForSession = async (code: any) => {
+      await supabase.auth.exchangeCodeForSession(code)
+      router.replace('/')
+    }
+
+    if (code) {
+      exchangeCodeForSession(code)
+    }
+
+    return () => {}
+  }, [router])
+
+  return (
+    <div className="h-screen w-screen flex justify-center">
+      <div className="flex-1 flex flex-col w-full max-w-sm justify-center gap-2">
+        <p className="text-center text-neutral-400">
+          You'll be redirected shortly...
+        </p>
+      </div>
+    </div>
+  )
+}
