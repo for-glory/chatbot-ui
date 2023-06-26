5334a3daeb131c7a842fe08775e7e1459f95188e
for-glory <forglory0905@outlook.com>
2023-06-26 03:47:33 -0700

    curious why it shows error when not returning null

--- pages/auth/callback/callback.tsx
+++ pages/auth/callback/callback.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-// import { useRouter } from 'next/navigation'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
@@ -23,13 +22,14 @@ export default function Callback() {
     return () => {}
   }, [router])
 
-  return (
-    <div className="h-screen w-screen flex justify-center">
-      <div className="flex-1 flex flex-col w-full max-w-sm justify-center gap-2">
-        <p className="text-center text-neutral-400">
-          You'll be redirected shortly...
-        </p>
-      </div>
-    </div>
-  )
+  // return (
+  //   <div className="h-screen w-screen flex justify-center">
+  //     <div className="flex-1 flex flex-col w-full max-w-sm justify-center gap-2">
+  //       <p className="text-center text-neutral-400">
+  //         You'll be redirected shortly...
+  //       </p>
+  //     </div>
+  //   </div>
+  // )
+  return null
 }
