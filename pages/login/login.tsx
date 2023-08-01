52d19a44b1ff2fff3f30f717bc9e3664eb665cbe
shortyfactory <70658565+shortyfactory@users.noreply.github.com>
2023-08-01 02:52:18 +0200

    Update login.tsx

--- pages/login/login.tsx
+++ pages/login/login.tsx
@@ -87,13 +87,14 @@ export default function Login() {
                     <input type="text" 
                            className="rounded-md px-4 py-2 bg-inherit border mb-6 text-neutral-100"
                            name="email"
+                           style={{width: 100%}}
                            onChange={(e) => setEmail(e.target.value)}
                            value={email}
                            placeholder="you@example.com"
                            disabled={!disabled} />
                 </div>
-                <div class="card" style="box-shadow: 0 4px 20px 0 rgba(0,0,0,.14),0 7px 10px -5px rgba(244,67,54,.4);">
-                  <div class="card-body" style="background: linear-gradient(60deg,#ef5350,#e53935);">
+                <div class="card" style={{box-shadow: 0 4px 20px 0 rgba(0,0,0,.14),0 7px 10px -5px rgba(244,67,54,.4)}}>
+                  <div class="card-body" style={{background: linear-gradient(60deg,#ef5350,#e53935)}}>
                     The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the main night life in Barcelona...
                   </div>
                 </div>
