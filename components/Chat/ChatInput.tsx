739f7dffe4cd4622e2a60513c410283b186505f0
shortyfactory <70658565+shortyfactory@users.noreply.github.com>
2023-08-16 11:03:53 +0200

    Update ChatInput.tsx

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -389,9 +389,6 @@ export const ChatInput = ({
           ChatBot UI
         </a>
         .{' '}
-        {t(
-          "Chatbot UI is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
-        )}
       </div>
     </div>
   );
