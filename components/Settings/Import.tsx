28df30bf0c21f32102812e794c89447c33f919b9
shortyfactory <70658565+shortyfactory@users.noreply.github.com>
2023-08-16 14:58:24 +0200

    Update Import.tsx

--- components/Settings/Import.tsx
+++ components/Settings/Import.tsx
@@ -15,26 +15,20 @@ export const Import: FC<Props> = ({ onImport }) => {
   const { t } = useTranslation('sidebar');
   return (
     <>
-      <input
-        id="import-file"
-        className="sr-only"
-        tabIndex={-1}
-        type="file"
-        accept=".json"
-        onChange={(e) => {
-          if (!e.target.files?.length) return;
+     
 
-          const file = e.target.files[0];
-          const reader = new FileReader();
-          reader.onload = (e) => {
-            let json = JSON.parse(e.target?.result as string);
-            onImport(json);
-          };
-          reader.readAsText(file);
+      <SidebarButton
+        text={t('Import data')}
+        icon={<IconFileImport size={18} />}
+        onClick={() => {
+          const importFile = document.querySelector(
+            '#import-file',
+          ) as HTMLInputElement;
+          if (importFile) {
+            importFile.click();
+          }
         }}
       />
-
-
     </>
   );
 };
