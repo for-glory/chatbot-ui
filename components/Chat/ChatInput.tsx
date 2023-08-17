8d73a3a6cd831df8f84ec6446570bc0a379a5930
shortyfactory <70658565+shortyfactory@users.noreply.github.com>
2023-08-17 05:04:26 +0200

    Update ChatInput.tsx

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -337,6 +337,7 @@ export const ChatInput = ({
 
           <button
             className="absolute right-2 top-2 rounded-sm p-1 text-neutral-800 opacity-60 hover:bg-neutral-200 hover:text-neutral-900 dark:bg-opacity-50 dark:text-neutral-100 dark:hover:text-neutral-200"
+            style={{color: '#fecc00' }}
             onClick={handleSend}
           >
             {messageIsStreaming ? (
