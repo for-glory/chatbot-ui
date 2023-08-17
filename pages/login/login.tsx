54ce70df7496b6959b921758a4c92a2b6d746192
shortyfactory <70658565+shortyfactory@users.noreply.github.com>
2023-08-17 13:15:42 +0200

    Update login.tsx

--- pages/login/login.tsx
+++ pages/login/login.tsx
@@ -25,7 +25,7 @@ export default function Login() {
         background: "linear-gradient(60deg,#ef535000,#e5393533)",
     },
     borderShadowStyles: {
-        border: "1px solid orange",
+        border: "1px solid #424141",
         paddingRight: "14px",
         color: "rgb(163 163 163/var(--tw-text-opacity))",
     },
