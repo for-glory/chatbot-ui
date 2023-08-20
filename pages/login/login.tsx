caecbb114e95014411326518254aae42ba1cfddc
shortyfactory <70658565+shortyfactory@users.noreply.github.com>
2023-08-21 00:39:07 +0200

    Update login.tsx

--- pages/login/login.tsx
+++ pages/login/login.tsx
@@ -3,6 +3,10 @@
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+
+import { createClient } from '@supabase/supabase-js'
+
+
 import Image from 'next/image'
 import { toast } from 'react-hot-toast'
 
@@ -13,7 +17,10 @@ export default function Login() {
   const [password, setPassword] = useState('')
   const [view, setView] = useState('sign-in-magic')
   const router = useRouter()
-  const supabase = createClientComponentClient()
+  
+const supabaseUrl = 'http://51.104.18.215'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 
   const Styles = {
